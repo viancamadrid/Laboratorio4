9142b0f567fd011739c6d6e6f6c5c52d5c39bfb2
viancamadrid <viancamadrid@unitec.edu>
2016-02-12 15:52:07 -0600

    llenar cubo

--- lab4.cpp
+++ lab4.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <cstdlib>
 
 using std::cout;
 using std::cin;
 using std::endl;
 
-int main(int argc, char*argv[]9{
-
+int main(int argc, char*argv[]){
+		
 	return 0;
+}
+
+void llenarCubo (int*** cubo){
+	for(int i=0; i<12; i++){
+		for (int j=0; j<12; j++){
+			for (int k=0; k<12; k++){
+				cubo[i][j][k]=0;
+			}
+		}
+	}
+	int r1, r2, r3;
+	srand(time(NULL));
+	for (int i=0; i<15; i++){
+		r1=1+rand()%(13-1);
+		r2=1+rand()%(13-1);
+		r3=1+rand()%(13-1);
+		cubo[r1][r2][r3]=1;	
+	}
+	return;
 }
