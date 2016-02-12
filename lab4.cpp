05f248681a706eb1c6b0a117b5796910db93ffde
Herbertpaz <herbert.paz@unitec.edu>
2016-02-12 15:41:20 -0600

    Crear y eliminar cubo

--- lab4.cpp
+++ lab4.cpp
@@ -3,8 +3,39 @@
 using std::cout;
 using std::cin;
 using std::endl;
+int*** crearCubo();
+void eliminarCubo(int***);
 
-int main(int argc, char*argv[]9{
+int main(int argc, char*argv[]){
 
 	return 0;
 }
+
+//funcion para crear el cubo
+int*** crearcubo(){
+	int*** cubo= new int**[12];
+	for(int i=0;i<12;i++){
+		cubo[i]= new int*[12];
+	}
+	for(int i=0;i<12;i++){
+		for(int j=0;j<12;j++){
+			cubo[i][j]= new int[12];
+		}	
+	}
+	return cubo;
+}
+//fin funcion para crear el cubo
+
+//funcion para eliminar el cubo
+void eliminarCubo(int*** cubo){
+	for(int i=0;i<12;i++){
+                for(int j=0;j<12;j++){
+                    	delete[] cubo[i][j];
+                } 
+        }
+	for(int i=0;i<12;i++){
+               	delete[] cubo[i];
+        }
+	delete[] cubo;
+}
+//fin funcion para eliminar el cubo 
